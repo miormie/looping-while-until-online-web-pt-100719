7ec2e40adad9f833e082b3c9aef0e3852bd9adc9
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-20 05:51:54 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,17 @@
 def using_while
+  
   levitation_force = 6
 	
+	while levitation_force < 10 
+	puts "Wingardium Leviosa"
+	
+	levitation_force += 1 
+
   #your code here
+  
+  # while that will continue to puts "Wingardium Leviosa" when it's less than 10 
+  # increment levitation by 1 
+ end  
 end
 
-
+#puts "Levitation force is #{using_while}!"
